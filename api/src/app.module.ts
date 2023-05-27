665f0f83fed9820928e82b446b6a8e51f5e12e3e
Marin Calin <marincalin2551@hotmail.com>
2023-05-27 13:56:55 +0300

    Containerized application

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,38 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {AppService} from "src/app.service";
+import {AppController} from "src/app.controller";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        console.log({
+          type: 'mysql',
+          host: configService.get('DB_HOST'),
+          port: +configService.get('DB_PORT'),
+          username: configService.get('DB_USER'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_NAME'),
+          entities: [],
+        });
+        return ({
+          type: 'mysql',
+          host: configService.get('DB_HOST'),
+          port: +configService.get('DB_PORT'),
+          username: configService.get('DB_USER'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_NAME'),
+          entities: [],
+        });
+      },
+      inject: [ConfigService]
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
